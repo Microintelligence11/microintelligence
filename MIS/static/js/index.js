cb661d248608d2d2afc681ffd7aea17779cb2ae4
microintelligence <microintelligence786@gmail.com>
2023-07-12 00:10:52 +0530

    Home Page Compelete

--- MIS/static/js/index.js
+++ MIS/static/js/index.js
@@ -1,13 +1,49 @@
 console.log("Hello")
 
+// #################### Number Counter code start here #############################################
 
-var swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      type: "fraction",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+const UpdateSectionCounter = document.querySelector('.Counter-Number');
+const workObserver = new IntersectionObserver((entries, observe) => {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) return;
+    const counterNumber = document.querySelectorAll(".Counter-Number");
+
+    const speed = 200;
+
+    counterNumber.forEach((curElem) => {
+        const updateNumber = () => {
+            const targetNumber = parseInt(curElem.dataset.numbers);
+            const intialNumber = parseInt(curElem.innerText);
+
+
+            const incrementNnumber = Math.trunc(targetNumber / speed);
+            if (intialNumber < targetNumber) {
+                curElem.innerText = `${intialNumber + incrementNnumber}+`;
+                setTimeout(updateNumber, 10);
+            }
+        };
+
+        updateNumber();
+    });
+
+}, {
+    root: null,
+    threshold: 0,
+});
+
+
+workObserver.observe(UpdateSectionCounter);
+
+
+// haburger start here 
+
+burger = document.querySelector('.burger');
+nanItem = document.querySelector('.nav-item');
+navbar = document.querySelector('.hidden');
+
+burger.addEventListener('click',()=>{
+    navbar.classList.toggle('hidden');
+});
+
+
